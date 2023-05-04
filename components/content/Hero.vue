446928f8bf94fcb755552376d6a6826140cb9406
Sébastien Chopin <seb@nuxt.com>
2023-05-04 16:54:20 +0200

    chore: update

--- components/content/Hero.vue
+++ components/content/Hero.vue
@@ -1,30 +1,56 @@
 <template>
-    <main class="container grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-12 md:pb-24">
-        <div class="py-6 md:order-1 hidden md:block">
-            <img src="/assets/hero.png" alt="Astronaut in the air" class="w-full h-full" />
-        </div>
-        <div>
-            <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight xl:tracking-tighter">
-              <ContentSlot :use="$slots.title" unwrap="p" />
-            </h1>
-            <p class="text-lg mt-4 text-slate-600 max-w-xl">
-              <ContentSlot :use="$slots.description" unwrap="p" />
-            </p>
-            <div class="mt-6 flex flex-col sm:flex-row gap-3">
-                <AppLink to="https://web3templates.com/templates/astroship-starter-website-template-for-astro"
-                    target="_blank"
-                    class="flex gap-1 items-center justify-center"
-                    rel="noopener">
-                    <Icon class="h-5 w-5 text-white" name="download" />
-                    Download for Free
-                </AppLink>
-                <AppLink sizes="lg" rel="noopener" to="https://github.com/surjithctly/astroship"
-                    class="flex gap-1 items-center justify-center "
-                    target="_blank">
-                    <Icon class="h-5 w-5 text-white" name="github" />
-                    GitHub Repo
-                </AppLink>
-            </div>
-        </div>
-    </main>
+  <main class="container grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-12 md:pb-24">
+    <div class="py-6 md:order-1 hidden md:block">
+      <img
+        src="/assets/hero.png"
+        alt="Astronaut in the air"
+        class="w-full h-full"
+      >
+    </div>
+    <div>
+      <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight xl:tracking-tighter">
+        <ContentSlot
+          :use="$slots.title"
+          unwrap="p"
+        >
+          Missing Title
+        </ContentSlot>
+      </h1>
+      <p class="text-lg mt-4 text-slate-600 max-w-xl">
+        <ContentSlot
+          :use="$slots.description"
+          unwrap="p"
+        >
+          Missing #description slot
+        </ContentSlot>
+      </p>
+      <div class="mt-6 flex flex-col sm:flex-row gap-3">
+        <AppLink
+          to="https://web3templates.com/templates/astroship-starter-website-template-for-astro"
+          target="_blank"
+          class="flex gap-1 items-center justify-center"
+          rel="noopener"
+        >
+          <Icon
+            class="h-5 w-5 text-white"
+            name="download"
+          />
+          Download for Free
+        </AppLink>
+        <AppLink
+          sizes="lg"
+          rel="noopener"
+          to="https://github.com/surjithctly/astroship"
+          class="flex gap-1 items-center justify-center "
+          target="_blank"
+        >
+          <Icon
+            class="h-5 w-5 text-white"
+            name="github"
+          />
+          GitHub Repo
+        </AppLink>
+      </div>
+    </div>
+  </main>
 </template>
--- components/content/Logos.vue
+++ components/content/Logos.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="container mt-24">
     <h2 class="text-center text-slate-500">
-      <ContentSlot :use="$slots.title" unwrap="p" />
+      <ContentSlot
+        :use="$slots.title"
+        unwrap="p"
+      />
     </h2>
     <div class="logos flex gap-8 md:gap-20 items-center justify-center mt-10 flex-wrap">
-      <ContentSlot :use="$slots.default" unwrap="p" />
+      <ContentSlot
+        :use="$slots.default"
+        unwrap="p"
+      />
     </div>
   </div>
 </template>
